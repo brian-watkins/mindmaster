977eeeb36edd75dc39f0e063229f092472d617a3
Brian Watkins <brian.watkins@gmail.com>
2019-12-11 21:58:28 -0500

    Removed tests — now we use specs!

--- tests/Game/Fakes/FakeCodeGenerator.elm
+++
@@ -1,8 +0,0 @@
-module Game.Fakes.FakeCodeGenerator exposing (..)
-
-import Elmer.Command as Command
-import Game.Types exposing (Color, Code)
-
-with : Code -> (Code -> msg) -> Cmd msg
-with code tagger =
-  Command.fake <| tagger code
